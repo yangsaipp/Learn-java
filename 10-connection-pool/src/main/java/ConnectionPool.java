6d569bb218c3d8c0f8291989a129225ab4ba91da
yangsai <yangsaipp@qq.com>
2018-05-17 11:12:24 +0800

    新增连接池

---
+++ 10-connection-pool/src/main/java/ConnectionPool.java
@@ -0,0 +1,50 @@
+import java.sql.Connection;
+import java.util.LinkedList;
+
+public class ConnectionPool {  
+        //Connection只是一个接口，具体实现是由数据库厂商决定的  
+        private LinkedList<Connection> pool = new LinkedList<Connection>() ;  
+          
+        public ConnectionPool(int initialSize) {  
+            if (initialSize > 0) {  
+                for (int i = 0; i < initialSize; i++) {  
+                   pool.addLast(ConnectionDriver.createConnection()) ;  
+                }  
+            }  
+        }  
+          
+        public void releaseConnection(Connection connection) {  
+            if (connection ! = null) {  
+                synchronized (pool) {  
+                    // 连接释放后需要进行通知， 这样其他消费者能够感知到连接池中已经归还了一个连接  
+                    pool.addLast(connection) ;  
+                    pool.notifyAll() ;  
+                }  
+            }  
+        }  
+          
+        // 在mills内无法获取到连接， 将会返回null  
+        public Connection fetchConnection(long mills) throws InterruptedException {  
+            synchronized (pool) {  
+                // 完全超时  
+                if (mills <= 0) {  
+                    while (pool. isEmpty() ) {  
+                        pool.wait() ;  
+                    }  
+                    return pool. removeFirst() ;  
+                } else {  
+                    long future = System.currentTimeMillis() + mills;  
+                    long remaining = mills;  
+                    while (pool.isEmpty() && remaining > 0) { //注意这里两个条件必须同时满足  
+                        pool.wait(remaining) ;  
+                        remaining = future - System.currentTimeMillis() ;  
+                    }  
+                    Connection result = null;  
+                    if (! pool.isEmpty() ) {  
+                        result = pool.removeFirst() ;  
+                    }  
+                    return result;  
+                }  
+            }  
+        }  
+    } 
