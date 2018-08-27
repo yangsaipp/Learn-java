fa296e021fe56adc6b02aaeba44e61cfeec1a35c
yangsai <yangsaipp@qq.com>
2018-08-27 23:52:08 +0800

    引入apache-pool

--- 10-connection-pool/README.md
+++ 10-connection-pool/README.md
@@ -1,8 +1,12 @@
-# SpringBoot - 标题
+# 池对象
 
 ## 疑问收集
 
-1. 连接池里如何实现获取超时？
+1. GenericObjectPool的borrowObject以及returnObject的逻辑？
+2. GenericObjectPool中borrowObject怎么实现获取超时？
+3. GenericObjectPool中如何定时驱逐多余的空闲池资源？
+4. GenericObjectPool中创建池资源时如何保证不超过最大的数量？
+5. GenericObjectPool中销毁池资源时如何保证保留指定的空闲数量？
 
 ## 重点摘要
 
---
+++ 10-connection-pool/src/main/java/apache/Main.java
@@ -0,0 +1,23 @@
+package apache;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.NoSuchElementException;
+
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.junit.Test;
+
+
+public class Main {
+
+	@Test
+	public void test() throws NoSuchElementException, IllegalStateException, Exception {
+		ObjectPool<StringBuffer> pool = new GenericObjectPool<StringBuffer>(new StringBufferFactory());
+		ReaderUtil readerUtil = new ReaderUtil(pool);
+		Reader in = new FileReader(this.getClass().getClassLoader().getResource("test.txt").getFile());
+		System.out.println("===:" + readerUtil.readToString(in));
+		System.out.println("===bb:" + pool.borrowObject());
+	}
+	
+}
