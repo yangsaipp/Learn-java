05da0a8ae6f11b3e0ee4617d103a1d29e123ab0a
yangsai <yangsaipp@qq.com>
2019-01-21 09:36:37 +0800

    测试更多数据类型的可见性

--- 16-thread/src/main/java/pool/TomcatApplication.java
+++ 16-thread/src/main/java/pool/TomcatApplication.java
@@ -1,5 +1,8 @@
 package pool;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * 不带volatile修饰符
  * 1. flag为Boolean类型存在可见性问题
- * 2. flag为Map类型不存在可见性问题
+ * 2. flag为Map<String, Boolean>类型不存在可见性问题
+ * 3. flag为String类型存在可见性问题
+ * 4. flag为对象（修改flag的成员变量）存在可见性问题
+ * 5. flag为Map<String, MyObject>类型不存在可见性问题
  * @author  杨赛
  * @since   jdk1.7
  * @version 2019年1月21日 杨赛
@@ -21,11 +27,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TomcatApplication {
 	private final static Logger logger = LoggerFactory.getLogger(TomcatApplication.class);
 	
-	public static Boolean flag = new Boolean(true);
+	public static Map<String,MyObject> flag = new HashMap<>();
+//	public static MyObject flag = new MyObject();
+//	public static String flag = "true";
+//	public static Boolean flag = new Boolean(true);
 //	public static Map<String,Boolean> flag = new HashMap<>();
-//	static {
+	static {
 //		flag.put("flag", true);
-//	}
+		flag.put("flag", new MyObject());
+	}
 	
 	private static int count = 0;
 	
@@ -35,8 +45,11 @@ public class TomcatApplication {
 		count ++;
 		if(count == 1) {
 			logger.info("flag之前：{}", flag.toString());
+			flag.get("flag").setFlag("false");
+//			flag.setFlag("false");
 //			flag.put("flag", false);
-			flag = new Boolean(false);
+//			flag = "false";
+//			flag = new Boolean(false);
 			logger.info("改变flag=false。");
 			logger.info("flag之后：{}", flag.toString());
 		}
@@ -48,7 +61,8 @@ public class TomcatApplication {
 	String test2() {
 		logger.info("=========test2 start ===");
 		int i = 0;
-		while (flag) {
+		while (flag.get("flag").getFlag().equals("true")) {
+//		while (flag) {
 //		while (flag.get("flag")) {
             i++;
         }
@@ -59,4 +73,23 @@ public class TomcatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TomcatApplication.class, args);
 	}
+	
+	static class MyObject {
+		public String flag = "true";
+
+		/**
+		 * @return 获取 flag属性值
+		 */
+		public String getFlag() {
+			return flag;
+		}
+
+		/**
+		 * @param flag 设置 flag 属性值为参数值 flag
+		 */
+		public void setFlag(String flag) {
+			this.flag = flag;
+		}
+		
+    }
 }
